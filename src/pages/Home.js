d7845f5f4e4dbc63303cae7016e1e0a9003c2b44
Ahmad Saad <64619851+saadpocalypse@users.noreply.github.com>
2024-04-21 13:42:44 +0500

    Responsive Done
    The website is now fully responsive. Only have to add the functionality to change colors now.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import styles from "../styles/Home.module.css";
-import { ArrowDownOutlined } from "@ant-design/icons";
+import { ArrowDownOutlined, MenuOutlined } from "@ant-design/icons";
 
 export default function Home() {
     return (
@@ -16,12 +16,13 @@ export default function Home() {
                     <p className={styles.activeLink}>Home</p>
                     <p className={styles.link}>Services</p>
                     <p className={styles.link}>Contact</p>
+                    <MenuOutlined className = {styles.burger}/>
                 </div>
             </div>
 
             <div className={styles.sectionTwo}>
                 <div className = {styles.sectionContent}>
-                    <div>
+                    <div className = {styles.sectionContentInnerDiv}>
                         <div className={styles.text}>
                             <p className={styles.header}>
                                 The easiest way to generate color pallettes for
@@ -41,6 +42,7 @@ export default function Home() {
                             viewBox="0 0 481 933"
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
+                            className = {styles.graphic}
                         >
                             <path
                                 d="M463.5 18H112.5C60.0329 18 17.5 60.533 17.5 113V113C17.5 165.467 60.033 208 112.5 208H240.5C363.659 208 463.5 307.84 463.5 431V834.75C463.5 879.347 427.347 915.5 382.75 915.5V915.5C338.153 915.5 302 879.347 302 834.75V502.25C302 423.688 238.313 360 159.75 360V360C81.1875 360 17.5 423.688 17.5 502.25V915.5"
