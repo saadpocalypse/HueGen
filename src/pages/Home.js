100cf2ce847c7aefe69a265929ec499f1e998f28
Ahmad Saad <64619851+saadpocalypse@users.noreply.github.com>
2024-04-22 12:35:12 +0500

    AntDesign Color Pickers added
    Have to set up the components to actually change the color of things

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import styles from "../styles/Home.module.css";
 import { ArrowDownOutlined, MenuOutlined } from "@ant-design/icons";
+import { ColorPicker, Space } from "antd";
 
 export default function Home() {
     return (
@@ -16,21 +17,23 @@ export default function Home() {
                     <p className={styles.activeLink}>Home</p>
                     <p className={styles.link}>Services</p>
                     <p className={styles.link}>Contact</p>
-                    <MenuOutlined className = {styles.burger}/>
+                    <MenuOutlined className={styles.burger} />
                 </div>
             </div>
 
             <div className={styles.sectionTwo}>
-                <div className = {styles.sectionContent}>
-                    <div className = {styles.sectionContentInnerDiv}>
+                <div className={styles.sectionContent}>
+                    <div className={styles.sectionContentInnerDiv}>
                         <div className={styles.text}>
                             <p className={styles.header}>
                                 The easiest way to generate color pallettes for
                                 your web or mobile application.
                             </p>
                             <p className={styles.tagLine}>
-                                Pick colors, see what they look like and click
-                                to copy the hexcodes.
+                                Pick colors, see what they look like and get the
+                                hexcodes.{" "}
+                                <span className={styles.linkBody}>Here</span> is
+                                an example of a link.
                             </p>
                         </div>
                         <button className={styles.button}>sample button</button>
@@ -42,7 +45,7 @@ export default function Home() {
                             viewBox="0 0 481 933"
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
-                            className = {styles.graphic}
+                            className={styles.graphic}
                         >
                             <path
                                 d="M463.5 18H112.5C60.0329 18 17.5 60.533 17.5 113V113C17.5 165.467 60.033 208 112.5 208H240.5C363.659 208 463.5 307.84 463.5 431V834.75C463.5 879.347 427.347 915.5 382.75 915.5V915.5C338.153 915.5 302 879.347 302 834.75V502.25C302 423.688 238.313 360 159.75 360V360C81.1875 360 17.5 423.688 17.5 502.25V915.5"
@@ -54,7 +57,46 @@ export default function Home() {
                         </svg>
                     </div>
                 </div>
-                <ArrowDownOutlined className = {styles.icon}/>
+                <ArrowDownOutlined className={styles.icon} />
+            </div>
+
+            <div className = {styles.sectionThree}>
+                <div className={styles.colorPickerDiv}>
+                    <p className = {styles.colorTitle}>Navbar</p>
+                    <ColorPicker defaultValue="#1677ff" size="large" showText />
+                </div>
+                <div className={styles.colorPickerDiv}>
+                    <p className = {styles.colorTitle}>Nav Links</p>
+                    <ColorPicker defaultValue="#1677ff" size="large" showText />
+                </div>
+                <div className={styles.colorPickerDiv}>
+                    <p className = {styles.colorTitle}>Body</p>
+                    <ColorPicker defaultValue="#1677ff" size="large" showText />
+                </div>
+                <div className={styles.colorPickerDiv}>
+                    <p className = {styles.colorTitle}>Primary Text</p>
+                    <ColorPicker defaultValue="#1677ff" size="large" showText />
+                </div>
+                <div className={styles.colorPickerDiv}>
+                    <p className = {styles.colorTitle}>Secondary Text</p>
+                    <ColorPicker defaultValue="#1677ff" size="large" showText />
+                </div>
+                <div className={styles.colorPickerDiv}>
+                    <p className = {styles.colorTitle}>Links</p>
+                    <ColorPicker defaultValue="#1677ff" size="large" showText />
+                </div>
+                <div className={styles.colorPickerDiv}>
+                    <p className = {styles.colorTitle}>Buttons</p>
+                    <ColorPicker defaultValue="#1677ff" size="large" showText />
+                </div>
+                <div className={styles.colorPickerDiv}>
+                    <p className = {styles.colorTitle}>Graphics</p>
+                    <ColorPicker defaultValue="#1677ff" size="large" showText />
+                </div>
+                <div className={styles.colorPickerDiv}>
+                    <p className = {styles.colorTitle}>Accents</p>
+                    <ColorPicker defaultValue="#1677ff" size="large" showText />
+                </div>
             </div>
         </div>
     );
